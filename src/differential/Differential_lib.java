58b55936733630ef3676103afdac003545be9394
50516021 <akira.musico@gmail.com>
2016-11-14 16:26:37 +0900

    20161107 submit

--- src/differential/Differential_lib.java
+++ src/differential/Differential_lib.java
@@ -24,11 +24,11 @@ public class Differential_lib implements Differential_if {
 	return Math.abs(getRx()-getDx())/getRx()*100;
 
 	}
+	//Input value of a> 1
+	//Input value of h> 0.00000001
+	//f(x)=x^2のx=1.0における微分係数は2.0
+	//f(x)=x^2のx=1.0における数値微分は1.999999987845058
+	//相対誤差：6.07747097092215E-7
+	//となり、一番いい時のhは0.00000001
 	
 }
-//Input value of a> 1
-//Input value of h> 0.00000001
-//f(x)=x^2のx=1.0における微分係数は2.0
-//f(x)=x^2のx=1.0における数値微分は1.999999987845058
-//相対誤差：6.07747097092215E-7
-//となり、一番いい時のhは0.00000001
